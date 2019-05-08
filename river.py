932a86910d2c6a2294863ef61c9a66196e9b793e
SONG <36039296+Masongyi@users.noreply.github.com>
2019-05-08 16:08:35 +0200

    Add files via upload

--- river.py
+++ river.py
@@ -24,8 +24,8 @@ from crazyflie_driver.msg import Position
 idx = 0
 move = 0
 wait = 1
-stop_threshold =8
-dist_threshold = 0.1
+stop_threshold =1
+dist_threshold = 0.5
 NPOINTS = 12
 callback_data = None
 odom = None
@@ -61,17 +61,81 @@ def pose_callback(msg):
             onepointpublish(rx1[idx],ry1[idx],yaw_degrees[idx])
             local_map_to_odom = map_to_odom(rx1[idx],ry1[idx])
             #print("local map to odom",local_map_to_odom)
+            d =np.array([0,0])
             d = np.array([local_map_to_odom.x-pos.x,local_map_to_odom.y-pos.y])
             print("d",d)
             print("pos.x",pos.x)
             distance = np.linalg.norm(d)
         #publish_cmd(idx)
             print("distance",distance)
+            if idx == 4:
+                distance = distance -0.70
+            if idx==5:
+               distance = distance - 0.4
+            if idx==6:
+                #dist_threshold=0.72
+                #distance = distance - 0.1
+                for i in range(40000000):
+                    if i > 39999988:
+                        print(i)
+                for i in range(45):
+                    d = distance -(i*0.01)
+
+                    #print(distance)
+                    if d < dist_threshold:
+                        print(d)
+                        distance = d
+                        break
+            if idx==7:
+                #2.5
+                #dist_threshold=0.72
+                for i in range(5000000):
+                    if i > 4999988:
+                        print(i)
+                for i in range(70):
+                    d = distance -(i*0.01)
+
+                    #print(distance)
+                    if d < dist_threshold:
+                        distance = d
+                        break
+            if idx==8:
+                #2.5
+                #dist_threshold=0.72
+                for i in range(5000000):
+                    if i > 4999988:
+                        print(i)
+                for i in range(150):
+                    d = distance -(i*0.01)
+
+                    #print(distance)
+                    if d < dist_threshold:
+                        distance = d
+                        break
+
+            if idx==9:
+
+                  #dist_threshold=0.72
+                  for i in range(2000000):
+                    if i > 1999988:
+                        print(i)
+                  for i in range(100):
+                      d = distance -(i*0.01)
+
+                      #print(distance)
+                      if d < dist_threshold:
+                          distance = d
+                          break
 
 
             if(distance < dist_threshold):
+                d =np.array([0,0])
+                distance = 0.0
                 print("reached at index",idx)
                 pose_callback.state = wait
+            # else:
+            #     print("let's wait")
+            #     pose_callback.state = wait
 
     elif pose_callback.state == wait:
         # Wait here for some iterations, counter here which will keep counting timpes
@@ -81,8 +145,9 @@ def pose_callback(msg):
         if (pose_callback.count > stop_threshold):
             #We've waited enough, start moving again to next point
             print("next goal")
-            pose_callback.count = 0
             pose_callback.state = move
+            pose_callback.count = 0
+
             idx = idx + 1
             print("idx inside nextgoal",idx)
             idx = idx % NPOINTS
@@ -93,23 +158,23 @@ def pose_callback(msg):
 
 def onepointpublish(x,y,yaw1):
     global odom
-    #rate = rospy.Rate(10)
+    rate = rospy.Rate(10)
     goal = PoseStamped()
     goal.header.stamp = rospy.Time.now()
     goal.header.frame_id = "map"
     goal.pose.position.x = x
     goal.pose.position.y = y
     goal.pose.position.z = 0.4
-    goal.pose.orientation.x = 0.0
-    goal.pose.orientation.y = 0.0
-    goal.pose.orientation.z = -0.999021480034635
-    goal.pose.orientation.w = -0.0442276206618389
+    # goal.pose.orientation.x = 0.0
+    # goal.pose.orientation.y = 0.0
+    # goal.pose.orientation.z = -0.999021480034635
+    # goal.pose.orientation.w = -0.0442276206618389
     # roll, pitch, yaw = euler_from_quaternion((goal.pose.orientation.x,
     #                                           goal.pose.orientation.y,
     #                                           goal.pose.orientation.z,
     #                                           goal.pose.orientation.w))
     #print(goal.header.stamp)
-    if not tf_buf.can_transform(goal.header.frame_id, 'cf1/odom', rospy.Time.now(), timeout=rospy.Duration(0.2)):
+    if not tf_buf.can_transform('map', 'cf1/odom', rospy.Time.now(), timeout=rospy.Duration(0.2)):
         rospy.logwarn_throttle(10.0, 'No transform from %s to cf1/odom' % goal.header.frame_id)
         return
 
@@ -148,6 +213,7 @@ def map_to_odom(x,y):
     goal.pose.position.x = x
     goal.pose.position.y = y
     goal.pose.position.z = 0.4
+
     #goal.pose.orientation.z = 0
     # if not tf_buf.can_transform(goal.header.frame_id, 'cf1/odom', goal.header.stamp):
     #     rospy.logwarn_throttle(10.0, 'No transform from %s to cf1/odom' % goal.header.frame_id)
@@ -198,9 +264,9 @@ def main():
     # rx1 =           [0.5, 2.0, 2.0, 2.0,2.0, 0.0,    0.0, 0.0]
     # ry1=            [0.0, 0.0, 0.0, 2.0,2.0, 2.0,   2.0, 0.0]
     # yaw_degrees =   [0.0, 0.0,90.0,90.0,180.0,180.0,270.0,270.0]
-    rx1 =             [0.5, 2.0, 2.0,  2.0,  2.0, 2.0,    1.5,  0.0,   0.0,   0.0,   0.0,  0.0]
-    ry1 =             [0.0, 0.0, 0.0,  0.5,  2.0, 2.0,    2.0,  2.0,   2.0,   1.5,   0.0,  0.0]
-    yaw_degrees =     [0.0, 0.0, 90.0, 90.0, 90.0,180.0, 180.0, 180.0, 270.0, 270.0, 270.0,360.0]
+    rx1 =             [0.5, 2.0, 2.0, 2.0, 2.0, 2.0, 1.5,    0.5,  0.0,   0.0,   0.0,  0.0]
+    ry1 =             [0.0, 0.0, 0.0, 0.5, 1.5, 1.5,  2.0,   2.0,  1.5,   1.0,   0.0,  0.0]
+    yaw_degrees =     [0.0, 0.0, 90.0,90.0,90.0, 180.0, 180.0, 180.0, 270.0, 270.0, 270.0,360.0]
 
     print(rx1)
     #while rospy
@@ -212,7 +278,7 @@ def main():
 
 if __name__ == '__main__':
     main()
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         if odom:
             #print("yahio")
